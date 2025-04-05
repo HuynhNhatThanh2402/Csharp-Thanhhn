dea6d9ab863aa070ffde35414761e0380253b275
HuynhNhatThanh2402 <23521440@gm.uit.edu.vn>
2025-04-05 23:19:22 +0700

    f

---
+++ Bai073/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Nhập giá trị x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nhập giá trị n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        double S = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            double Mau = i * (i + 1) / 2.0;
+
+            double Tu = Math.Pow(x, i);
+
+            S += Tu / Mau;
+        }
+
+        Console.WriteLine($"Tổng S = {S}");
+    }
+}
---
+++ Bai3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        CultureInfo viVN = new CultureInfo("vi-VN");
+        Thread.CurrentThread.CurrentCulture = viVN;
+        Thread.CurrentThread.CurrentUICulture = viVN;
+
+        while (true)
+        {
+            DateTime now = DateTime.Now;
+
+            string thu = viVN.DateTimeFormat.GetDayName(now.DayOfWeek);
+            string ngay = now.ToString("d/M/yyyy");
+            string gio = now.ToString("H:mm:ss");
+
+            Console.WriteLine($"Thời gian hiện tại: {thu}, ngày {ngay}, thời gian: {gio}");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
